9df5ed5d539b26d74bbdda3c22b3b842dac51fd1
NielsAlston <nielsalston@gmail.com>
2015-10-19 17:02:43 +1100

    Added some functions to pull data for the graph, not working yet

--- routes/account.js
+++ routes/account.js
@@ -6,15 +6,41 @@ var Session = require('../models/session.js');
 
 
 router.get('/', function(req, res) {
-  res.render('account', {chips: req.session.user.chips, chipTracker: req.session.user.chipTracker});
+  if (req.isAuthenticated()) {
+    res.render('account', {chips: req.user.chips});
+  } else {
+    res.render('account', {chips: req.session.user.chips});
+  }
+  
+});
+
+router.get('/profitability', function(req, res, next) {
+  
+  if (req.isAuthenticated()) {
+    res.json({chipsArray: req.user.chipTracker.change, datesArray: req.user.chipTracker.date});
+  } else {
+    res.json({chipsArray: 0, datesArray: 0});
+  }
+  
 });
 
 router.post('/topup-chips', function(req, res, next) {
   
-  if (req.session.user.chips < 2000) {
-    req.session.user.chips = 2000;
+  if (req.isAuthenticated()) {
+    // console.log(req.user.chips);
+    if (req.user.chips < 2000) {
+      req.user.chips = 2000;
+      // console.log(req.user.chips);
+    }
+    res.json({chips: req.user.chips});
+  } else {
+    if (req.session.user.chips < 2000) {
+      req.session.user.chips = 2000;
+      // console.log(req.session.user.chips);
+    }
+    res.json({chips: req.session.user.chips});
   }
-  res.json({chips: req.session.user.chips});
+  
 });
 
 module.exports = router;
