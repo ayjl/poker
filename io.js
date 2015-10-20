9f76372a50fdf48d7cab85d9018b800538286b04
Adrian <adrian.lin@student.unsw.edu.au>
2015-10-21 00:09:16 +1100

    Back-end for chat
    * POK-31

--- io.js
+++ io.js
@@ -1,13 +1,52 @@
+var Chat = require('./models/chat.js');
+
 module.exports = function(io) {
   io.on('connection', function(socket) {
-    io.emit('chat message', 'A user connected');
+    if(socket.request.session.passport.user) {
+      var userID = socket.request.session.passport.user;
+      var newUser = onlineUsers.add(userID, socket.id);
+
+      if(newUser) {
+        socket.broadcast.emit('user connect', userID);
+      }
+
+      socket.emit('online list', onlineUsers.getUserIDs());
+      socket.join(userID);
+    }
 
     socket.on('disconnect', function(){
-      io.emit('chat message', 'A user disconnected');
+      var userID = socket.request.session.passport.user;
+
+      if(onlineUsers.removeSocket(userID, socket.id)) {
+        onlineUsers.removeUserTimeout(userID, socket);
+      }
     });
 
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
+    socket.on('send message', function(friendID, msg) {
+      var fromID = socket.request.session.passport.user;
+
+      var chat = new Chat({ 
+          from: fromID
+        , to: friendID
+        , message: msg
+      });
+
+      chat.save()
+      .then(function() {
+        socket.broadcast.to(friendID).emit('receive message', fromID, msg);
+      });
+    });
+
+    socket.on('open chat', function(friendID) {
+      var userID = socket.request.session.passport.user;
+      onlineUsers.addOpenChats(userID, friendID);
+      socket.broadcast.to(userID).emit('open chat', friendID);
+    });
+
+    socket.on('close chat', function(friendID) {
+      var userID = socket.request.session.passport.user;
+      onlineUsers.removeOpenChats(userID, friendID);
+      socket.broadcast.to(userID).emit('close chat', friendID);
     });
   });
 
